0f06ccb37dd4997874b00a7b6f1cc6e63f4a94cb
Hiep <hiep.damhai@vietis.com.vn>
2020-05-27 18:15:42 +0700

    using useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
-import ColorBox from "./components/ColorBox";
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
+import PostList from "./components/PostList";
 
 function App() {
 	const [todoList, setTodoList] = useState([
@@ -10,6 +10,32 @@ function App() {
 		{ id: 2, title: "hihihihi" },
 		{ id: 3, title: "heheheheeh" }
 	]);
+
+	const [postList, setPostList] = useState([]);
+
+	useEffect(() => {
+		async function fetchPostList() {
+			try {
+				const requestURL =
+					"http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1";
+				const respone = await fetch(requestURL);
+				const responeJSON = await respone.json();
+				console.log({ responeJSON });
+
+				const { data } = responeJSON;
+				setPostList(data);
+			} catch (error) {
+				console.log("Fail to load data!");
+			}
+		}
+
+		console.log("Post List");
+		fetchPostList();
+	}, []);
+
+	useEffect(() => {
+		console.log("TODO List");
+	});
 
 	function handleTodoClick(todo) {
 		const index = todoList.find(x => x.id === todo.id);
@@ -21,20 +47,28 @@ function App() {
 	}
 
 	function handleTodoFormSubmit(formValues) {
-		const newTodo = {
-			id: todoList.length + 1,
-			...formValues
-		};
-		const newTodoList = [...todoList];
-		newTodoList.push(newTodo);
-		setTodoList(newTodoList);
+		if (formValues.title !== "") {
+			const newTodo = {
+				id: todoList.length + 1,
+				...formValues
+			};
+			const newTodoList = [...todoList];
+			newTodoList.push(newTodo);
+			setTodoList(newTodoList);
+		} else {
+			window.alert("Please input title!");
+		}
 	}
 
 	return (
 		<div className="app">
-			<h1>TodoList</h1>
+			<h1>Todo</h1>
+
 			<TodoForm onSubmit={handleTodoFormSubmit} />
 			<TodoList todos={todoList} onTodoClick={handleTodoClick} />
+
+			<h1>List</h1>
+			<PostList posts={postList} />
 		</div>
 	);
 }
